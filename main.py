d552cc63815998e0365dda7187c2d0a2794ade72
Sarik raj Karki <eros@Sariks-Mac-mini.local>
2025-02-07 20:38:33 -0800

    Added more structure to main function

--- main.py
+++ main.py
@@ -1,53 +1,116 @@
+import heapq as min_heap
+
+from eight_puzzle.Node import Node
+# from eight_puzzle.queueingFunction import QueueingFunction
+
 # hardcoded puzzles;
 # add matrix's of puzzle
-a = [[1, 3, 2], [4, 5, 6], [8, 9, 0]]
-b = 1
-c = 1
-d = 1
-e = 1
+a = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
+b = [[1, 2, 3], [5, 0, 6], [4, 7, 8]]
+c = [[1, 3, 6], [5, 0, 2], [4, 7, 8]]
+d = [[1, 3, 6], [5, 0, 7], [4, 8, 2]]
+e = [[1, 6 ,7], [5, 0, 3], [4, 8, 2]]
+f = [[7, 1, 2], [4, 8, 5], [6, 3, 0]]
+g = [[0, 7, 2], [4, 6, 1], [3, 5, 8]]
 
 
 def main():
-    puzzleMode = input("Enter 1 for default puzzle, 2 to make your own puzzle")
-    if puzzleMode == "1":
+    puzzleMode = int(input("Enter 1 for default puzzle, 2 to make your own puzzle"))
+    if puzzleMode == 1:
         selectAlgorithm(defaultPuzzleSelect())
-    if puzzleMode == "2":
-        # ask for puzzle information
-        formedPuzzle = 1;
-        selectAlgorithm(formedPuzzle)
+
+    # if puzzleMode == "2":
+    #     # ask for puzzle information
+    #     formedPuzzle = 1;
+    #     selectAlgorithm(formedPuzzle)
 
 
 def selectAlgorithm(puzzleToSolve):
-    selectedAlgorithm = input(
-        "Enter 1 for Uniform Cost Search, 2 for A* with the Misplaced Tile heuristic and 3 for A* with the Manhattan Distance heuristic: ")
+    selectedAlgorithm = int(input("Enter 1 for Uniform Cost Search, 2 for A* with the Misplaced Tile heuristic and 3 for A* with the Manhattan Distance heuristic: "))
     if selectedAlgorithm == 1:
         uniformCostSearch(puzzleToSolve, 0)
     if selectedAlgorithm == 2:
-        misplacedTileHeuristic(puzzleToSolve, 1)
+        uniformCostSearch(puzzleToSolve, 1)
     if selectedAlgorithm == 3:
-        manhattanDistanceHeuristic(puzzleToSolve, 2)
+        uniformCostSearch(puzzleToSolve, 2)
 
 
 def defaultPuzzleSelect():
-    choicePuzzle = input("Enter the default puzzle you would like to select; 1: Easy, 2: Medium, 3: Hard ")
-    if choicePuzzle == "1":
+    choicePuzzle = int(input("Enter the default puzzle you would like to select; 1: Easy, 2: Medium, 3: Hard "))
+    if choicePuzzle == 1:
         return a
-    if choicePuzzle == "2":
+    if choicePuzzle == 2:
         return b
+    if choicePuzzle == 3:
+        return c
+    if choicePuzzle == 4:
+        return d
+    if choicePuzzle == 5:
+        return e
+    if choicePuzzle == 6:
+        return f
+    if choicePuzzle == 7:
+        return g
 
 
 def uniformCostSearch(puzzle, heuristic):
+    #goal-state
+
+    goalState= [[1, 2, 3], [4,5,6], [7,8,0]]
+
+    startingState = Node(puzzle)
+
+    if heuristic == 0:
+        startingState.algorithm = "UCS"
+
+    if heuristic == 1:
+        startingState.algorithm = "MST"
+
+    if heuristic == 2:
+        startingState.algorithm = "MNH"
+
+    queueStack = []
+    printStack = []
+    seenList = {}
+
+    maxQueueSize = 0
+
+    min_heap.heappush(queueStack, startingState)
+
+    seenList[tuple(map(tuple, startingState.data))] = True
 
 
-    solvedPuzzle = 1
-    print(solvedPuzzle)
+    while  len(queueStack) > 0:
+        maxQueueSize = max(maxQueueSize, len(queueStack))
+        currentNode = min_heap.heappop(queueStack)
+
+        if currentNode.data == goalState:
+            # while len(printStack) > 0:
+            #     printStack.pop()
+            #     print()
+            # print()
+            print("Solution found")
+            print(f"Goal state was found at depth {currentNode.depth}")
+            print(f"Number of Nodes expanded: {currentNode.numNodeExpanded}")
+            print(f"Max Queue Size: {maxQueueSize}")
+            return
+        # for i in range(3):
+        #     for j in range(3):
+        #         print(currentNode.data[i][j], end=" ")
+        #     print()  # Add a newline between states
+        printStack.append(currentNode.data)
+        currentNode.expand(queueStack, seenList)
 
 
-def misplacedTileHeuristic(puzzle, heuristic):
-    solvedPuzzle = 1
-    print(solvedPuzzle)
+main()
 
 
-def manhattanDistanceHeuristic(puzzle, heuristic):
-    solvedPuzzle = 1
-    print(solvedPuzzle)
+
+
+
+
+
+
+
+
+
